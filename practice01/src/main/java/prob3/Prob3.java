6bfc7006581250a9c9c66f3134f1bf98a2ac2641
jin0kim326 <jin0kim326@gmail.com>
2021-09-06 19:22:30 +0900

    test commit

--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -7,7 +7,7 @@ public class Prob3 {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-//hello
+//hello 22222222
 		System.out.print("숫자를 입력하세요 : ");
 		int inputNum = scanner.nextInt();
 		
