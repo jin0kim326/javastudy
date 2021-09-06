76a23a5952aa0a9888964999f0accb1222ed6cdf
jin0kim326 <jin0kim326@gmail.com>
2021-09-06 20:20:38 +0900

    practice01 complete

--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -5,13 +5,33 @@ import java.util.Scanner;
 public class Prob3 {
 	
 	public static void main(String[] args) {
-
+		//even , odd
 		Scanner scanner = new Scanner(System.in);
-//hello 22222222
+		int result = 0;
+		
 		System.out.print("숫자를 입력하세요 : ");
 		int inputNum = scanner.nextInt();
 		
-		System.out.print(inputNum);
+		
+		if ((inputNum % 2) ==0 ) {
+		//짝수
+		for (int i=0; i<=inputNum; i++) {
+			if((i%2)==0) {
+				result = result + i;
+			}
+		}
+			
+		}else {
+		for (int i=0; i<=inputNum; i++) {
+			if((i%2)!=0) {
+				result = result + i;
+			}
+		}
+
+			
+		}
+		
+		System.out.print("결과값 : " + result);
 		scanner.close();
 	}
 }
--- practice01/src/main/java/prob5/Prob5.java
+++ practice01/src/main/java/prob5/Prob5.java
@@ -3,5 +3,12 @@ package prob5;
 public class Prob5 {
 
 	public static void main(String[] args) {
+		int max=100;
+		
+		for (int num=1; num <100; num++) {
+			if ((num%3)==0) {
+				System.out.println(num+"짝");
+			}
+		}
 	}
 }
