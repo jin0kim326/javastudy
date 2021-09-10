300c3f39a59664d6b9077799d066ea52a89f5405
jin0kim326 <53967482+jin0kim326@users.noreply.github.com>
2021-09-10 10:40:27 +0900

    test commit (mac)

--- practice01/src/main/java/prob1/Prob1.java
+++ practice01/src/main/java/prob1/Prob1.java
@@ -6,7 +6,7 @@ public class Prob1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
 		
-		System.out.print("수를 입력하시오!!! : ");
+		System.out.print("수를 입력하시오@@@ : ");
 		int num = scanner.nextInt();
 		
 		if((num % 3) == 0) {
