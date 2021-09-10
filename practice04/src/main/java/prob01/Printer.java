d7f7878d8ecdab18f95171ba922fc63c64f8de5d
jin0kim326 <53967482+jin0kim326@users.noreply.github.com>
2021-09-10 19:56:59 +0900

    prac04-1

---
+++ practice04/src/main/java/prob01/Printer.java
@@ -0,0 +1,23 @@
+package prob01;
+
+public class Printer {
+	
+	
+	public void println(int integer) {
+		System.out.println(integer);	
+	}
+	
+	public void println(boolean bool) {
+		System.out.println(String.valueOf(bool));
+	}
+	
+	public void println(double db) {
+		System.out.println(db);
+	}
+	
+	public void println(String str) {
+		System.out.println(str);
+	}
+	
+	
+}
