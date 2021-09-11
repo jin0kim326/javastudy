1a1122ba77c890bc3e08f70b20569f84e49d5617
jin0kim326 <53967482+jin0kim326@users.noreply.github.com>
2021-09-11 20:53:37 +0900

    prac04-3

---
+++ practice04/src/main/java/prob03/Unit.java
@@ -0,0 +1,21 @@
+package prob03;
+
+public class Unit {
+	private int x;
+	private int y;
+	
+	public Unit(int x, int y) {
+	//유닛의 현위치 .
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void move(int x, int y) {
+		// 좌표로 이동 .
+	}
+	
+	public void stop() {
+		// 정지 .
+	}
+
+}
