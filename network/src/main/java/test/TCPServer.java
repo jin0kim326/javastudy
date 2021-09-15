b303c32b093fa226d3d98843883bdcac121a60df
jin0kim326 <53967482+jin0kim326@users.noreply.github.com>
2021-09-15 14:08:54 +0900

    tcp sever test

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -1,9 +1,12 @@
 package test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class TCPServer {
 
@@ -16,7 +19,7 @@ public class TCPServer {
 			 //2. 바인딩(binding) 
 			 //   Socket의 InetSocketAddress(IPAdress + port)
 			 //  IPAdress : 0.0.0.0 (모든 IP로 부터의 연결허용)
-			 serverSocket.bind(new InetSocketAddress("0.0.0.0", 2613));
+			 serverSocket.bind(new InetSocketAddress("0.0.0.0", 5000));
 			 
 			 //3. accept
 			 //	  클라이언트로 부터 연결 요청을 기다림.
@@ -27,7 +30,39 @@ public class TCPServer {
 			 System.out.println("[server] Connected by client[" + remoteHostAddress + ":" + remoteHostPort +  "]");
 			 
 			 
-			 System.out.println("accepted...");
+			 try {
+				 //4. IO Stream 받아오기
+				 InputStream is = socket.getInputStream();
+				 OutputStream os = socket.getOutputStream();
+				 
+				 while (true) {
+					 //5.  데이터 읽기
+					 byte[] buffer = new byte[256];
+					 int readByteCount = is.read(buffer); 		// blocking
+					 
+					 if (readByteCount == -1) {
+						 // 클라이언트가 정상적으로 종료( close() 호출 )
+						 System.out.println("[server] closed by client");
+						 break;
+					 }
+					 String data = new String ( buffer, 0, readByteCount, "utf-8");
+					 System.out.println("[server] received : " + data);
+				 }
+			 } catch (SocketException e) {
+				 System.out.println("[server] suddenly closed by client : "+ e);
+			 } catch (IOException e) {
+				 System.out.println("[server] Error : "+ e);
+			 } finally {
+				 try {
+					 if(socket != null && !socket.isClosed() ) {
+						 socket.close();						 
+					 }
+				 } catch (IOException e) {
+					 e.printStackTrace();
+				 }
+				 
+			 }
+			 
 		} catch (IOException e) {
 			System.out.println("[Server] Error : "+e);
 		} finally {
