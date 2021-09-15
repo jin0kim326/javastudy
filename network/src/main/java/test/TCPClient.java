9b16489e10b3bd969a39dfbddf0230c3ba5cb480
jin0kim326 <53967482+jin0kim326@users.noreply.github.com>
2021-09-15 14:19:27 +0900

    TCP Client #1

--- network/src/main/java/test/TCPClient.java
+++ network/src/main/java/test/TCPClient.java
@@ -1,9 +1,35 @@
 package test;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 public class TCPClient {
-
+	private static final String SERVER_IP = "127.0.0.1";
+	private static final int SERVER_PORT = 5000;
+	
 	public static void main(String[] args) {
-
+		Socket socket = null;
+		
+		try {
+			// 1. 소켓 생성 
+			socket = new Socket();
+			
+			// 2. 서버 연결
+			socket.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT));
+		} catch (SocketException e) {
+			System.out.println("[client] suddenly closed by server" + e);
+		} catch (IOException e) {
+			System.out.println("[client] error : " + e);
+		} finally {
+			 try {
+				 if(socket != null && !socket.isClosed() ) {
+					 socket.close();						 
+				 }
+			 } catch (IOException e) {
+				 e.printStackTrace();
+			 }
+		 }
 	}
 
 }
