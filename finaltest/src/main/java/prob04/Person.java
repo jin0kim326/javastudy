65d67135f891a5b59606a652a679332e52378617
jin0kim326 <53967482+jin0kim326@users.noreply.github.com>
2021-09-19 12:32:22 +0900

    finaltest 4

--- finaltest/src/main/java/prob04/Person.java
+++ finaltest/src/main/java/prob04/Person.java
@@ -5,5 +5,26 @@ public class Person {
 	private int age;
 	private String name;
 	
-	/* 코드 작성 */
+	public Person () { 
+		this(12,"");
+	}
+	
+	public Person ( String name) {
+		this(12, name);		
+	}
+	
+	
+	public Person (int age, String name) {
+		this.age = age;	
+		this.name = name;
+		numberOfPerson +=1;	// 생성자호출시 인구수 1증가 
+	}
+	
+	public void selfIntroduce() {
+		System.out.println("내이름은 "+ name +" 이며, 나이는 "+ age +"살입니다.");
+	}
+	
+	public static int getPopulation() { 
+		return numberOfPerson;
+	}
 }
